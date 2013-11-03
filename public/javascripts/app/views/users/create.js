b54f762c88244b8a37b9245bd7b20a1caf661b2d
Ivan Nosov <i.k.nosov@gmail.com>
2013-11-03 21:53:40 +0200

    create user

--- public/javascripts/app/views/users/create.js
+++ public/javascripts/app/views/users/create.js
@@ -1,9 +1,44 @@
-define(['backbone', 'hbs!templates/users/create'], function(Backbone, template) {
+define(['backbone', 'underscore', 'hbs!templates/users/create'], function(Backbone, _, template) {
     return Backbone.View.extend({
         el: '#content',
         template: template,
+        events: {
+            "submit form": "save"
+        },
+
+        initialize: function() {
+            this.listenTo(this.model, 'error', this.renderErrors);
+        },
+
         render: function() {
             this.$el.html(this.template(this.model.toJSON()))
+        },
+
+        renderErrors: function(model, xhr) {
+            if (xhr.status !== 400) { return }
+            var errors = xhr.responseJSON;
+
+            this.$('.help-block').remove();
+            this.$('.has-error').removeClass('has-error');
+            _.each(errors, function(error) {
+                var input = $('[name="' + error.path + '"]');
+                input.parents('.form-group').addClass('has-error');
+                Backbone.$('<span>', { "class": "help-block", text: error.type })
+                    .insertAfter(input)
+            })
+        },
+
+        save: function(event) {
+            var model = this.model
+              , form = this.$(event.target);
+
+            _.each(form.serializeArray(), function(item) {
+                model.set(item.name, item.value)
+            });
+
+            model.save();
+
+            return false
         }
     })
 });
