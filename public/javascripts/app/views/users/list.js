0ad2b224e77ea4b7d4206dd6a24b78a94ebf8929
Ivan Nosov <i.k.nosov@gmail.com>
2013-11-06 12:13:12 +0200

    draw pagnation

--- public/javascripts/app/views/users/list.js
+++ public/javascripts/app/views/users/list.js
@@ -1,8 +1,16 @@
-define(['backbone', 'hbs!templates/users/list'], function(Backbone, template) {
+define(['backbone', 'hbs!templates/users/list', 'views/pagination'], function(Backbone, template, PaginationView) {
     return Backbone.View.extend({
         template: template,
         render: function() {
+            var pagination = new PaginationView({
+                itemsPerPage: this.collection.itemsPerPage,
+                currentPage: this.collection.currentPage,
+                totalItems: this.collection.totalItems,
+                url: '#list/:page'
+            });
+
             this.$el.html(this.template(this.collection.toJSON()));
+            this.$('.table-pagination').html(pagination.render().el);
             return this
         }
     })
