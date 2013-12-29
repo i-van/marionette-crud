5a8e3949e77e2c2593bda0b5c716cfee58ae940e
Ivan Nosov <i.k.nosov@gmail.com>
2013-12-29 10:51:04 +0200

    render index page via jade

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ require('./models/user');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -30,6 +32,9 @@ if ('development' == app.get('env')) {
 
 // user resource
 var user = require('./routes/user');
+app.get('/', function(req, res) {
+    res.render('index');
+});
 app.get('/api/users', user.list);
 app.get('/api/users/:id', user.show);
 app.put('/api/users/:id', user.update);
