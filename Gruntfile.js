afd40ff8e8b23d3b951030e51861454bf5fe0cbb
Ivan Nosov <i.k.nosov@gmail.com>
2013-12-29 10:52:17 +0200

    build app

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+
+module.exports = function(grunt) {
+
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        requirejs: {
+            compile: {
+                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
+                options: {
+                    baseUrl: 'public/javascripts/app',
+                    mainConfigFile: 'public/javascripts/app/main.js',
+                    out: 'public/javascripts/app/main.build.js',
+                    name: 'main',
+
+                    optimize: 'uglify2',
+                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
+                    // https://github.com/yeoman/grunt-usemin/issues/30
+                    //generateSourceMaps: true,
+                    // required to support SourceMaps
+                    // http://requirejs.org/docs/errors.html#sourcemapcomments
+                    preserveLicenseComments: false,
+                    useStrict: true,
+                    wrap: true,
+                    uglify2: {} // https://github.com/mishoo/UglifyJS2
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+        'requirejs'
+    ]);
+};
