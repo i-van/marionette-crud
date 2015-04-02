641802d6d5b2047fafe3733c9be5063842d63227
Ivan Nosov <i.k.nosov@gmail.com>
2015-04-02 11:38:15 +0300

    user editing

--- public/javascripts/routes/users.js
+++ public/javascripts/routes/users.js
@@ -1,6 +1,6 @@
 define(
-['marionette', 'models/user', 'collections/users', 'views/users/home', 'views/users/list', 'views/users/create'],
-function(Marionette, User, Users, HomeView, ListLayout, CreateView) {
+['marionette', 'models/user', 'collections/users', 'views/users/home', 'views/users/list', 'views/users/create', 'views/users/edit'],
+function(Marionette, User, Users, HomeView, ListLayout, CreateView, EditView) {
     return Marionette.AppRouter.extend({
         initialize: function(options) {
             this.app = options.app;
@@ -9,7 +9,7 @@ function(Marionette, User, Users, HomeView, ListLayout, CreateView) {
             "": "homeAction",
             "list(/:page)": "listAction",
             "create": "createAction",
-            "edit/:id": "edit",
+            "edit/:id": "editAction",
             "remove/:id": "remove"
         },
 
@@ -33,6 +33,16 @@ function(Marionette, User, Users, HomeView, ListLayout, CreateView) {
         createAction: function() {
             this.app.rootView.navRegion.currentView.activateLink('create');
             this.app.rootView.contentRegion.show(new CreateView({ model: new User() }));
+        },
+
+        editAction: function(id) {
+            var contentRegion = this.app.rootView.contentRegion;
+
+            new User({ _id: id }).fetch({
+                success: function(user) {
+                    contentRegion.show(new EditView({ model: user }));
+                }
+            });
         }
     });
 });
--- public/javascripts/views/users/edit.js
+++ public/javascripts/views/users/edit.js
@@ -1,12 +1,12 @@
 define(
-['backbone', 'underscore', 'views/users/create', 'hbs!templates/users/edit', 'helpers/alert'],
-function(Backbone, _, CreateView, template, Alert) {
+['backbone', 'underscore', 'views/users/create', 'hbs!templates/users/edit', 'helpers/popup'],
+function(Backbone, _, CreateView, template, popup) {
     return CreateView.extend({
         template: template,
 
         navigate: function() {
-            Alert.success('User was successfully updated');
-            Backbone.history.navigate('list', { trigger: true })
+            popup.alert('Success', 'User was successfully updated');
+            Backbone.history.navigate('list', { trigger: true });
         }
     })
 });
